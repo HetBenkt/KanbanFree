268e0168ccd58c2ded7f904382f20bef975261be
HetBenkt <HetBenkt@gmail.com>
2017-09-19 15:51:42 +0200

    Initial commit

---
+++ src/main/java/nl/bos/KanbanfreeApplication.java
@@ -0,0 +1,12 @@
+package nl.bos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanfreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanfreeApplication.class, args);
+	}
+}
