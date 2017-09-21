b997a050427ebe11297f2860fa58f78e4ab36bd9
HetBenkt <hetbenkt@gmail.com>
2017-09-21 21:14:27 +0200

    Fixed the custom controller so it's available from postman

--- src/main/java/nl/bos/controllers/MemberController.java
+++ src/main/java/nl/bos/controllers/MemberController.java
@@ -10,19 +10,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * Created by bosa on 21-9-2017.
  */
-@RepositoryRestController
+@RestController
 @RequestMapping("/api")
 public class MemberController {
     @Autowired
     IMemberService memberService;
 
     @GetMapping("/member/{id}")
-    Member getMember(@PathVariable("id") final long id) {
+    Member getMemberById(@PathVariable("id") final long id) {
         return memberService.findById(id);
     }
+
+//    @GetMapping("/member/{lastName}")
+//    List<Member> getMemberByName(@PathVariable("lastName") final String lastName) {
+//        return memberService.findByLastName(lastName);
+//    }
 
     @RequestMapping("/")
     String defaultMapping() {
